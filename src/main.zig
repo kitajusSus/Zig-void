babaf32c524713ca3eac21e9e2c0431cc4251fc1
kitajuSus <kitaj2458@gmail.com>
2025-10-31 21:23:19 +0100

    zig 1

--- src/main.zig
+++ src/main.zig
@@ -1,15 +1,1 @@
 const std = @import("std");
-const Zig_void = @import("Zig_void");
-
-pub fn main() !void {
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-    try Zig_void.bufferedPrint();
-}
-
-test "simple test" {
-    const gpa = std.testing.allocator;
-    var list: std.ArrayList(i32) = .empty;
-    defer list.deinit(gpa); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(gpa, 42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
